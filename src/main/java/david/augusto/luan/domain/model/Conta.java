ec27a95f4f50ac9d2734abb9f90d973f71fc75b2
luan-alencar <luanalencar134@gmail.com>
2020-11-12 11:28:43 -0300

    adicionando construtores

--- src/main/java/david/augusto/luan/domain/model/Conta.java
+++ src/main/java/david/augusto/luan/domain/model/Conta.java
@@ -4,12 +4,23 @@ import lombok.*;
 
 @Data
 @Getter @Setter
+@AllArgsConstructor
 public abstract class Conta {
 
 	private int agencia;
 	private int conta;
 	private String titular;
 	private double saldo;
+	
+	public Conta() {
+	}
+
+	public Conta(int agencia, int conta, String titular) {
+		this.agencia = agencia;
+		this.conta = conta;
+		this.titular = titular;
+		this.saldo = 0;
+	}
 	
 	
 }
